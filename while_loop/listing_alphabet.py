363f8598b574fef6ae571d0ecc65808f4055794c
Benjamin Ghouzi <bghouzi.2022@lfny.org>
2017-03-16 22:13:02 +0000

    init

---
+++ while_loop/listing_alphabet.py
@@ -0,0 +1,58 @@
+num = 0
+while num < 26:
+    if num == 0:
+        print("a")
+    elif num == 1:
+        print("b")
+    elif num == 2:
+        print("c")
+    elif num == 3:
+        print("d")
+    elif num == 4:
+        print("e")
+    elif num == 5:
+        print("f")
+    elif num == 6:
+        print("g")
+    elif num == 7:
+        print("h")
+    elif num == 8:
+        print("i")
+    elif num == 9:
+        print("j")
+    elif num == 10:
+        print("k")
+    elif num == 11:
+        print("l")
+    elif num == 12:
+        print("m")
+    elif num == 13:
+        print("n")
+    elif num == 14:
+        print("o")
+    elif num == 15:
+        print("p")
+    elif num == 16:
+        print("q")
+    elif num == 17:
+        print("r")
+    elif num == 18:
+        print("s")
+    elif num == 19:
+        print("t")
+    elif num == 20:
+        print("u")
+    elif num == 21:
+        print("v")
+    elif num == 22:
+        print("w")
+    elif num == 23:
+        print("x")
+    elif num == 24:
+        print("y")
+    elif num == 25:
+        print("z")
+    elif num == 26:
+        print("k")
+    num = num + 1
+    
---
+++ while_loop/listing_number.py
@@ -0,0 +1,4 @@
+num = 0
+while num <= 26:
+    print(num)
+    num = num + 1
